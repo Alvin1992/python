df3f8357af46147d99959f6be1f2a98458709dcb
Alvin <fanrx_1992@163.com>
2016-10-08 18:54:20 +0800

    complete basic functions

--- addressBook.py
+++ addressBook.py
@@ -2,7 +2,10 @@
 # -*- coding: UTF-8 -*-
 # Filename: addressBook.py
 
-import time, cPickle, sys, os
+import time
+import cPickle
+import sys
+import os
 
 # 版本信息
 info = {}
@@ -19,9 +22,37 @@ def updateTime():
 
 def addContact(book, name, addr):
     """Add a contact to addressBook"""
-    book[name] = addr
+    try:
+        result = book[name]
+        book[name] = addr
+        print 'Change contact information of %s successfully from %s to %s' % (name, result, addr)
+    except KeyError:
+        book[name] = addr
+        print 'Add contact information of %s successfully' % name
+
+
+def findContact(book, name):
+    try:
+        result = book[name]
+        print 'The address of %s is %s' % (name, result)
+        sys.exit()
+    except KeyError:
+        print 'No contact information of %s' % name
+        sys.exit()
+
+
+def delContact(book, name):
+    try:
+        del book[name]
+        print 'Delete contact of %s successfully' % name
+    except KeyError:
+        print 'No contact information of %s' % name
+
 
 if __name__ == '__main__':
+    if os.path.exists(dataPath):
+        fileObject = file(dataPath)
+        addressBook = cPickle.load(fileObject)
     args = sys.argv
     length = len(args)
 
@@ -34,14 +65,21 @@ if __name__ == '__main__':
     firstArg = args[1]
     # 没有短命令或者长命令的情况
     if length == 2:
-        result = addressBook[firstArg]
-        if result:
-            print 'The address of %s is' % (firstArg, result)
-        else:
-            print 'No contact information of %s' % firstArg
+        findContact(addressBook, firstArg)
 
     # 第一个参数如果以-开始，表示进行的是增删改查
-    # if firstArg.startswith('-'):
-    #     if firstArg[1:] == 'a':
-    #         addContact(addressBook, args[2], args[3])
-    #     sys.exit()
+    if firstArg.startswith('-'):
+        option = firstArg[1:]
+        if option == 's':
+            findContact(addressBook, args[2])
+        elif option == 'a' or option == 'c':
+            addContact(addressBook, args[2], args[3])
+        elif option == 'd':
+            delContact(addressBook, args[2])
+    else:
+        if length == 3:
+            addContact(addressBook, args[1], args[2])
+
+    f = file(dataPath, 'w')
+    cPickle.dump(addressBook, f)
+    f.close()
