4450d53f866c101a703487b4836275f01096958f
Alvin <fanrx_1992@163.com>
2016-10-13 10:48:27 +0800

    qiushi

--- crawler/qiushi.py
+++ crawler/qiushi.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# pip安装cssselector,lxml,pyquery
-# 安装lxml需要vs c++ for python和lxml.whl文件
 from pyquery import PyQuery as pq
 import urllib2
+import sys
 
-req = urllib2.Request('http://www.qiushibaike.com/hot/page/3/')
+req = urllib2.Request('http://www.qiushibaike.com/hot/page/2/')
 req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; WOW64)')
 response = urllib2.urlopen(req)
 d = pq(response.read())
 
-foot = d('.content span')
-
-for item in foot:
-    print (d(item).html().replace('<br />', '\n') + '\n\n')
+allBlocks = d('.article')
+for block in allBlocks:
+    content = d(block).find('.content span').html()
+    comment = d(block).find('.main-text').html()
+    print ('\n' + content.replace('<br />', '\n') + '\n')
+    if not comment:
+        print '没有神评论'
+    else:
+        print '神评论 %s' % comment
+    cmd = str(raw_input('next?'))
+    if cmd == 'quit':
+        sys.exit()
