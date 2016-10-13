62e44fc3414ab946e413a30a89592584f0f2429c
Alvin1992 <fanrx_1992@163.com>
2016-10-13 22:53:35 +0800

    qiushi simple version

--- crawler/qiushi.py
+++ crawler/qiushi.py
@@ -8,17 +8,26 @@ import sys
 req = urllib2.Request('http://www.qiushibaike.com/hot/page/2/')
 req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; WOW64)')
 response = urllib2.urlopen(req)
-d = pq(response.read())
+d = pq(response.read().decode('utf-8'))
 
 allBlocks = d('.article')
 for block in allBlocks:
-    content = d(block).find('.content span').html()
-    comment = d(block).find('.main-text').html()
-    print ('\n' + content.replace('<br />', '\n') + '\n')
+    block = d(block)
+    content = block.find('.content span').html()
+    author = block.find('.author h2').html()
+    comment = block.find('.main-text').html()
+    upvote = block.find('.stats-vote i').html()
+    commentNum = block.find('.stats-comments i').html()
+    print u'\n-----------作者：%s 好评：%s 评论数：%s-------------' % (author, upvote, commentNum)
+    try:
+        print ('\n' + content.replace('<br />', '\n') + '\n')
+    except UnicodeEncodeError, e:
+        print u'编码错误'
+    print u'-----------------------------------------------------------------'
     if not comment:
-        print '没有神评论'
+        print u'\n没有神评论'
     else:
-        print '神评论 %s' % comment
-    cmd = str(raw_input('next?'))
+        print u'\n神评论 %s' % comment
+    cmd = str(raw_input(u'\nquit or enter \n'))
     if cmd == 'quit':
         sys.exit()
